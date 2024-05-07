b68a9a8aef5eab3563c1ad9c99c5cb816a224d39
abdo0302 <abdelljabarhamri06@gmail.com>
2024-05-08 00:07:30 +0200

    coding index.php front end

--- index.php
+++ index.php
@@ -3,10 +3,195 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/index.css">
+    <title>Blog news</title>
+    <!--logo de page-->
+    <link rel="icon" href="./img/logo.png">
+    <!--font-awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--style css-->
+    <link rel="stylesheet" href="./css/home.css">
 </head>
 <body>
+    <!--============[[header start]]=================-->
+    <header>
+        <div class="contenar_nav_bar">
+            <a href="#" class="logo">Blog<span>News</span></a>
+            <nav>
+                <ul>
+                    <li><a class="acteve" href="#">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Catégories</a></li>
+                    <li><a href="#">Authors</a></li>
+                    <li><a href="#"><i class="fa-solid fa-right-to-bracket"></i> SIGN UP</a></li>
+                    <li><a href="#"><i class="fa-solid fa-circle-user"></i> SIGN IN</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!--header end -->
+
+    <!--============[[main start]]=================-->
+    <main>
+        <!--============{{section de home start}}============-->
+        <section id="home">
+            <div>
+                <p>A comprehensive news site where you follow the latest developments in Arab and international events around the clock</p>
+                <a href="#">TechnOur programsology</a>
+            </div>
+        </section>
+        <!--section de home end-->
+
+        <!--============{{section de Recent articles start}}============-->
+        <section class="contenar_articles">
+            <h5>Recent articles</h5>
+            <div class="contenar_de_Recent_articles">
+                <!-- les card-->
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+            </div>
+        </section>
+        <!--section de Recent articles end-->
+
+        <!--============{{section de Specialized categories start}}============-->
+        <section class="contenar_articles">
+            <h5>Specialized categories</h5>
+            <div class="contenar_de_Recent_articles">
+                <!-- les card-->
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+            </div>
+        </section>
+        <!--section de Specialized categories end-->
+
+        <!--============{{section de News and events start}}============-->
+        <section class="contenar_articles">
+            <h5>News and events</h5>
+            <div class="contenar_de_Recent_articles">
+                <!-- les card-->
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+                <div class="card">
+                    <img src="./img/images.jpg" alt="">
+                    <div class="div1"><span class="date">17 Jan 2022</span> <span class="nome_categori"></span>catégori</div>
+                    <span class="tetle">lorem There is no one who</span>
+                    <span class="button_read_more">Read more</span>
+                </div>
+            </div>
+        </section>
+        <!--section de News and events end-->        
+    </main>
+    <!--main end -->
+
+    <!--============[[footer start]]=================-->
+    <footer>
+        <div class="section_footer">
+            <!--abute footer-->
+            <div class="abute_footer"> 
+                <a href="#" class="logo_footer">Blog<span>News</span></a>
+                <p>Welcome to the News section of our blog! Here you will find the latest articles and the most exciting news in different areas. We provide you with comprehensive coverage of global and local events, as well as in-depth analysis and varied opinions on current issues. Discover the latest news and stay constantly informed of everything that is happening in the world of current news and developments.</p>
+           </div>
+            <!--nav footer-->
+            <div class="nav_footer">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">catégories</a></li>
+                    <li><a href="#">Auteurs</a></li>
+                </ul>
+            </div>
+            <!--icone social media footer-->
+            <div class="social_media">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-discord"></i>
+            <i class="fa-brands fa-twitter"></i>
+            </div>
+        </div>
+            <!--bar coperite-->
+        <div class="bar_coperite">
+            <span>All rights reserved © 2024</span>
+        </div>
+    </footer>
+    <!--footer end -->
+
     <script src="./js/index.js"></script>
 </body>
 </html>
--- utilisateur.php
+++ utilisateur.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--logo de page-->
+    <link rel="icon" href="./img/logo.png">
     <link rel="stylesheet" href="./css/utilisateur.css">
 </head>
 <body>
